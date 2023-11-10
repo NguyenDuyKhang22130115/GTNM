14664502167b386d63e6de1203e86b75b8e65c31
NguyenDuyKhang22130115 <22130115@st.hcmuaf.edu.vn>
2023-11-11 00:59:35 +0700

    mnwem

--- Lab_4/implement/javascript.js
+++ Lab_4/implement/javascript.js
@@ -70,28 +70,42 @@ function addFruit() {
   
 
   function editFruit() {
-    // Lấy hàng đang được sửa
+    // Get the table row that was clicked.
     const row = event.target.closest('tr');
   
-    // Lấy ID của hàng đang được sửa
-    const id = row.querySelector('td:nth-child(1)').textContent;
-  
-    // Lấy dữ liệu của hàng đang được sửa
+    // Get the fruit data from the table row.
+    const id = row.querySelector('td:first-child').textContent;
     const name = row.querySelector('td:nth-child(2)').textContent;
     const price = row.querySelector('td:nth-child(3)').textContent;
     const quantity = row.querySelector('td:nth-child(4)').textContent;
   
-    // Cập nhật dữ liệu trong biểu mẫu
-    document.querySelector('.form_item input[type="name"]').value = name;
-    document.querySelector('.form_item input[type="price"]').value = price;
-    document.querySelector('.form_item input[type="quantity"]').value = quantity;
+    // Set the input values to the fruit data.
+    document.querySelector('input[type="name"]').value = name;
+    document.querySelector('input[type="price"]').value = price;
+    document.querySelector('input[type="quantity"]').value = quantity;
   
-    // Ẩn nút Sửa, hiện nút Lưu
-    const editButton = document.querySelector('.form .edit-btn');
-    editButton.hidden = true;
-    const saveButton = document.querySelector('.form .add-btn');
-    saveButton.hidden = false;
+    // Show the save button and hide the add button.
+    document.querySelector('.save-btn').hidden = false;
+    document.querySelector('.add-btn').hidden = true;
   }
+  
+  function saveFruit() {
+    // Get the fruit data from the inputs.
+    const name = document.querySelector('input[type="name"]').value;
+    const price = document.querySelector('input[type="price"]').value;
+    const quantity = document.querySelector('input[type="quantity"]').value;
+  
+    // Update the table row with the new fruit data.
+    const row = document.querySelector('tr td:first-child');
+    row.parentNode.querySelector('td:nth-child(2)').textContent = name;
+    row.parentNode.querySelector('td:nth-child(3)').textContent = price;
+    row.parentNode.querySelector('td:nth-child(4)').textContent = quantity;
+  
+    // Hide the save button and show the add button.
+    document.querySelector('.save-btn').hidden = true;
+    document.querySelector('.add-btn').hidden = false;
+  }
+  
   
 
   function deleteFruit() {
@@ -114,43 +128,43 @@ function addFruit() {
   }
   
 
-function saveFruit() {
-    // Lấy các giá trị từ biểu mẫu
-    const name = document.querySelector('.form_item input[type="name"]').value;
-    const price = document.querySelector('.form_item input[type="price"]').value;
-    const quantity = document.querySelector('.form_item input[type="quantity"]').value;
+// function saveFruit() {
+//     // Lấy các giá trị từ biểu mẫu
+//     const name = document.querySelector('.form_item input[type="name"]').value;
+//     const price = document.querySelector('.form_item input[type="price"]').value;
+//     const quantity = document.querySelector('.form_item input[type="quantity"]').value;
   
-    // Lấy ID của hàng đang được sửa
-    const id = document.querySelector('.form_item input[type="id"]').value;
+//     // Lấy ID của hàng đang được sửa
+//     const id = document.querySelector('.form_item input[type="id"]').value;
   
-    // Tạo một đối tượng trái cây mới
-    const fruit = {
-      id,
-      name,
-      price,
-      quantity,
-    };
+//     // Tạo một đối tượng trái cây mới
+//     const fruit = {
+//       id,
+//       name,
+//       price,
+//       quantity,
+//     };
   
-    // Cập nhật dữ liệu trong bảng
-    const tableBody = document.querySelector('.list tbody');
-    const rows = tableBody.querySelectorAll('tr');
+//     // Cập nhật dữ liệu trong bảng
+//     const tableBody = document.querySelector('.list tbody');
+//     const rows = tableBody.querySelectorAll('tr');
   
-    for (let i = 0; i < rows.length; i++) {
-      const row = rows[i];
+//     for (let i = 0; i < rows.length; i++) {
+//       const row = rows[i];
   
-      // Nếu ID của hàng đang được sửa khớp với ID của hàng hiện tại
-      if (row.querySelector('td:nth-child(1)').textContent === id) {
-        // Cập nhật dữ liệu của hàng hiện tại
-        row.querySelector('td:nth-child(2)').textContent = name;
-        row.querySelector('td:nth-child(3)').textContent = price;
-        row.querySelector('td:nth-child(4)').textContent = quantity;
-      }
-    }
+//       // Nếu ID của hàng đang được sửa khớp với ID của hàng hiện tại
+//       if (row.querySelector('td:nth-child(1)').textContent === id) {
+//         // Cập nhật dữ liệu của hàng hiện tại
+//         row.querySelector('td:nth-child(2)').textContent = name;
+//         row.querySelector('td:nth-child(3)').textContent = price;
+//         row.querySelector('td:nth-child(4)').textContent = quantity;
+//       }
+//     }
   
-    // Ẩn nút Lưu, hiện nút Thêm
-    const saveButton = document.querySelector('.form .add-btn');
-    saveButton.hidden = true;
-    const addButton = document.querySelector('.form .add-btn[hidden]');
-    addButton.hidden = false;
-  }
+//     // Ẩn nút Lưu, hiện nút Thêm
+//     const saveButton = document.querySelector('.form .add-btn');
+//     saveButton.hidden = true;
+//     const addButton = document.querySelector('.form .add-btn[hidden]');
+//     addButton.hidden = false;
+//   }
   
